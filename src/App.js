abf81bb722c231a4ff3b96edb6f0c3777822eb93
Daniel Laurin <daniellaurin21@gmail.com>
2023-11-05 13:09:16 -0500

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>twitter clone</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
